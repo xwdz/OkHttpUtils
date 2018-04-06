3c0c61286a1f20f0f6998c4fca9624c111e96db5
xwdz <xwdz9989@gamil.com>
2018-04-06 21:14:11 +0800

    开发分支添加测试代码

--- app/src/main/java/com/xwdz/test/MainActivity.java
+++ app/src/main/java/com/xwdz/test/MainActivity.java
@@ -5,9 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.githang.android.snippet.security.DigestUtil;
+import com.xwdz.okhttpgson.StringCallBack;
 import com.xwdz.okhttpgson.JsonCallBack;
 import com.xwdz.okhttpgson.OkHttpRun;
-import com.xwdz.okhttpgson.StringCallBack;
 
 import okhttp3.Call;
 
@@ -15,6 +16,8 @@ import okhttp3.Call;
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final String POST = "http://iop.parkingwang.com:9397/iop/auth/login"; //
+    private static final String GET = "https://api.github.com/users";
     private TextView mTextView;
 
     @Override
@@ -28,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void get(View view) {
-        OkHttpRun.get("")
+        OkHttpRun.get(GET)
                 .execute(new StringCallBack() {
                     @Override
                     public void onSuccess(Call call, String response) {
@@ -43,8 +46,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void post(View view) {
-        OkHttpRun.post("").addParams("name", "11")
-                .addParams("pwd", "123")
+        OkHttpRun.post(POST).addParams("username", "irainiop_hxw")
+                .addParams("password", DigestUtil.doDigest("MD5", "000000"))
                 .execute(new JsonCallBack<WebToken>() {
 
                     @Override
