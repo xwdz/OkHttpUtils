47eb9e6898af890253c5a898bf9be9d4cfcca4fd
xwdz <xwdz9989@gamil.com>
2018-04-06 21:12:45 +0800

    修改测试代码

--- app/src/main/java/com/xwdz/test/TestApp.java
+++ app/src/main/java/com/xwdz/test/TestApp.java
@@ -2,8 +2,6 @@ package com.xwdz.test;
 
 import android.app.Application;
 
-import com.xwdz.okhttpgson.HttpManager;
-
 /**
  * @author huangxingwei(xwdz9989@gmail.com)
  * @since 2018/4/3
@@ -13,6 +11,5 @@ public class TestApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        HttpManager.getInstance().setOkHttpClient();
     }
 }
